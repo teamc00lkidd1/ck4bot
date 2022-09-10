f434cff0ae0daabf6f7c5e1785c26ccfb21c6655
lolpippiou <50070094+lolpippiou@users.noreply.github.com>
2022-09-10 15:23:49 +0200

    a

--- index.js
+++ index.js
@@ -89,15 +89,3 @@ client.on("interactionCreate", async interaction => {
 });
 
 client.login(process.env['token']);
-
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
-});
